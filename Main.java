983380757cf7dd50b7bd55477f2cc8a6a6d278b5
Tobitastik <144216903+Tobitastik@users.noreply.github.com>
2024-02-01 13:33:58 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,6 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world 2!");
         Random randomNum = new Random();
         int randomNumber = randomNum.nextInt(100) + 1;
 
